772dd53eb7889fc9ed30765f8f7ba94c0795c2d2
Joshua Anderson <j@joshua-anderson.com>
2016-06-17 11:05:29 -0700

    ref(version): remove unneeded APIVersion const that was moved to sdk (#86)

--- version/version.go
+++ version/version.go
@@ -3,9 +3,6 @@ package version
 // Version identifies this Deis product revision.
 const Version = "2.1.0-dev"
 
-// APIVersion identifies the latest Deis api version
-const APIVersion = "2.0"
-
 // BuildVersion is the git revision of the build.
 // Note: This value is overwritten by the linker during build
 var BuildVersion = ""
