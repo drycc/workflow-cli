2862f056e278996a74bea224ec246366c5952528
Joshua Anderson <j@joshua-anderson.com>
2016-07-19 11:28:41 -0700

    feat(colors): reserve magenta for controller log messages (#132)

--- cmd/colors_unix.go
+++ cmd/colors_unix.go
@@ -4,20 +4,33 @@ package cmd
 
 import "fmt"
 
+const colorStringEscape = "\033[3%dm"
+
 // Choose an ANSI color by converting a string to an int.
+//
+// Color 5, magenta, reserved for controller log messages.
+//
+// Colors 0 and 7, black and white, are skipped because they are likely to be unreadable
+// against terminal backgrounds. Instead, 9, the default text color is used, likely to be the color
+// of the two that is readable against the terminal background.
 func chooseColor(input string) string {
+	if input == "INFO" {
+		return fmt.Sprintf(colorStringEscape, 5)
+	}
+
 	var sum uint8
 
 	for _, char := range []byte(input) {
 		sum += uint8(char)
 	}
 
-	// Seven possible terminal colors
-	color := (sum % 7) + 1
+	// Eight possible terminal colors, but Black and White are excluded
+	color := (sum % 6) + 1
 
-	if color == 7 {
+	// Color 5 is reserved, replace it with default text color.
+	if color == 5 {
 		color = 9
 	}
 
-	return fmt.Sprintf("\033[3%dm", color)
+	return fmt.Sprintf(colorStringEscape, color)
 }
