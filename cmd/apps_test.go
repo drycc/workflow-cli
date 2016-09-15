1b06646873214d77150908338eea84a336bac1c4
Matt Tucker <ultimateboy@gmail.com>
2016-09-15 11:31:59 -0600

    tests(apps): test for apps:info, apps:destroy, and apps:transfer (#235)

--- cmd/apps_test.go
+++ cmd/apps_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/deis/workflow-cli/pkg/git"
 	"github.com/deis/workflow-cli/pkg/testutil"
+	"github.com/deis/workflow-cli/settings"
 )
 
 type expandURLCases struct {
@@ -103,6 +104,161 @@ func TestAppsListLimit(t *testing.T) {
 	assert.NoErr(t, err)
 	assert.Equal(t, b.String(), `=== Apps (1 of 2)
 lorem-ipsum
+`, "output")
+}
+
+func TestAppsInfo(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+    "uuid": "c4aed81c-d1ca-4ff1-ab89-d2151264e1a3",
+    "id": "lorem-ipsum",
+    "owner": "dolar-sit-amet",
+    "structure": {
+      "cmd": 1
+    },
+    "created": "2016-08-22T17:40:16Z",
+    "updated": "2016-08-22T17:40:16Z"
+}`)
+	})
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/pods/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+    "count": 1,
+    "results": [
+      {
+        "state": "up",
+        "started": "2016-08-22T17:42:16Z",
+        "name": "lorem-ipsum-cmd-1911796442-48b58",
+        "release": "v2",
+        "type": "cmd"
+      }
+    ]
+}`)
+	})
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/domains/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+    "count": 1,
+    "next": null,
+    "previous": null,
+    "results": [
+      {
+         "owner": "dolar-sit-amet",
+         "created": "2016-08-22T17:40:16Z",
+         "updated": "2016-08-22T17:40:16Z",
+         "app": "lorem-ipsum",
+         "domain": "lorem-ipsum"
+      }
+    ]
+}`)
+	})
+
+	s, err := settings.Load(cmdr.ConfigFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	url, err := cmdr.appURL(s, "lorem-ipsum")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if url == "" {
+		url = fmt.Sprintf(noDomainAssignedMsg, "lorem-ipsum")
+	}
+
+	err = cmdr.AppInfo("lorem-ipsum")
+	assert.NoErr(t, err)
+	assert.Equal(t, b.String(), `=== lorem-ipsum Application
+updated:  2016-08-22T17:40:16Z
+uuid:     c4aed81c-d1ca-4ff1-ab89-d2151264e1a3
+created:  2016-08-22T17:40:16Z
+url:      `+url+`
+owner:    dolar-sit-amet
+id:       lorem-ipsum
+
+=== lorem-ipsum Processes
+--- cmd:
+lorem-ipsum-cmd-1911796442-48b58 up (v2)
+
+=== lorem-ipsum Domains
+lorem-ipsum
+
+`, "output")
+}
+
+func TestAppDestroy(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+    "uuid": "c4aed81c-d1ca-4ff1-ab89-d2151264e1a3",
+    "id": "lorem-ipsum",
+    "owner": "dolar-sit-amet",
+    "structure": {
+      "cmd": 1
+    },
+    "created": "2016-08-22T17:40:16Z",
+    "updated": "2016-08-22T17:40:16Z"
+}`)
+	})
+	err = cmdr.AppDestroy("lorem-ipsum", "bad-confirm-string")
+	assert.Equal(t, err.Error(), `App lorem-ipsum does not match confirm bad-confirm-string, aborting.`, "output")
+
+	err = cmdr.AppDestroy("lorem-ipsum", "lorem-ipsum")
+	assert.NoErr(t, err)
+	assert.Equal(t, testutil.StripProgress(b.String()), `Destroying lorem-ipsum...
+done in 0s
+`, "output")
+}
+
+func TestAppTransfer(t *testing.T) {
+	t.Parallel()
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/apps/lorem-ipsum/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+    "uuid": "c4aed81c-d1ca-4ff1-ab89-d2151264e1a3",
+    "id": "lorem-ipsum",
+    "owner": "dolar-sit-amet",
+    "structure": {
+      "cmd": 1
+    },
+    "created": "2016-08-22T17:40:16Z",
+    "updated": "2016-08-22T17:40:16Z"
+}`)
+	})
+
+	err = cmdr.AppTransfer("lorem-ipsum", "test-user")
+	assert.NoErr(t, err)
+	assert.Equal(t, testutil.StripProgress(b.String()), `Transferring lorem-ipsum to test-user... done
 `, "output")
 }
 
