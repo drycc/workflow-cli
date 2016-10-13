6c9c9352cc402ca99882fbd2d9080c297e4193fc
Matt Tucker <ultimateboy@gmail.com>
2016-10-13 14:28:35 -0600

    tests(auth): add test for whoami --all (#261)

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -181,7 +181,7 @@ func TestWhoami(t *testing.T) {
   "groups": [],
   "user_permissions": [],
   "last_login": "2016-09-12T22:15:26Z",
-  "date_joined": "2016-09-12T22:15:26Z",
+  "date_joined": "2015-09-12T22:15:26Z",
   "is_active": true
 }`)
 	})
@@ -189,6 +189,50 @@ func TestWhoami(t *testing.T) {
 	err = cmdr.Whoami(false)
 	assert.NoErr(t, err)
 	expected := fmt.Sprintf("You are test at %s\n", server.Server.URL)
+	assert.Equal(t, b.String(), expected, "output")
+}
+
+func TestWhoamiAll(t *testing.T) {
+	t.Parallel()
+
+	cf, server, err := testutil.NewTestServerAndClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer server.Close()
+	var b bytes.Buffer
+	cmdr := DeisCmd{WOut: &b, ConfigFile: cf}
+
+	server.Mux.HandleFunc("/v2/auth/whoami/", func(w http.ResponseWriter, r *http.Request) {
+		testutil.SetHeaders(w)
+		fmt.Fprintf(w, `{
+  "email": "test@example.com",
+  "username": "test",
+  "first_name": "",
+  "last_name": "",
+  "is_superuser": true,
+  "is_staff": true,
+  "groups": [],
+  "user_permissions": [],
+  "last_login": "2016-09-12T22:15:26Z",
+  "date_joined": "2015-09-12T22:15:26Z",
+  "is_active": true
+}`)
+	})
+
+	err = cmdr.Whoami(true)
+	assert.NoErr(t, err)
+	expected := fmt.Sprintf(`ID: 0
+Username: test
+Email: test@example.com
+First Name: 
+Last Name: 
+Last Login: 2016-09-12T22:15:26Z
+Is Superuser: true
+Is Staff: true
+Is Active: true
+Date Joined: 2015-09-12T22:15:26Z
+`)
 	assert.Equal(t, b.String(), expected, "output")
 }
 
